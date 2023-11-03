9a723161182f8013a58657883abee0684a0a25c6
Ib soro <soro4827@gmail.com>
2023-11-03 22:16:27 +0000

    fix

--- apps/setup_env.sh
+++ apps/setup_env.sh
@@ -1,6 +1,6 @@
-echo DEBUG=${{secrets.DEBUG}} >> .env
+echo DEBUG=$DEBUG >> .env
 
-echo OVH_IP_ADDRESS= ${{ secrets.OVH_IP_ADDRESS }} >> .env
+echo OVH_IP_ADDRESS=$OVH_IP_ADDRESS >> .env
 echo DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]" >> .env
-echo OVH_USER=${{ secrets.OVH_USER }} >> .env
-echo OVH_PRIVATE_KEY=${{ secrets.OVH_PRIVATE_KEY }} >> .env
+echo OVH_USER=$OVH_USER >> .env
+echo OVH_PRIVATE_KEY=$OVH_PRIVATE_KEY >> .env
