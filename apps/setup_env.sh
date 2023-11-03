70e91351861b14df551e4a19aa3ed6c258eb760d
Ib soro <soro4827@gmail.com>
2023-11-03 22:25:25 +0000

    cofirm

--- apps/setup_env.sh
+++ apps/setup_env.sh
@@ -1,6 +1,4 @@
-echo DEBUG=$DEBUG >> .env
-
-echo OVH_IP_ADDRESS=$OVH_IP_ADDRESS >> .env
-echo DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]" >> .env
-echo OVH_USER=$OVH_USER >> .env
-echo OVH_PRIVATE_KEY=$OVH_PRIVATE_KEY >> .env
+echo OVH_IP_ADDRESS=$OVH_IP_ADDRESS >> ./apps/.env
+echo DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]" >> ./apps/.env
+echo OVH_USER=$OVH_USER >> ./apps/.env
+echo OVH_PRIVATE_KEY=$OVH_PRIVATE_KEY >> ./apps/.env
